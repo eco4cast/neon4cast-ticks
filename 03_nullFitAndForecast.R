e71b2be9a5889772371888052c84e381634300cf
EFI <efi>
2022-03-27 18:51:45 +0000

    fix the spelling too

--- 03_nullFitAndForecast.R
+++ 03_nullFitAndForecast.R
@@ -83,7 +83,7 @@ create_ensembles <- function(df, nmc = 500, forecast.year = 2021) {
     as_tibble() %>%
     mutate(year = forecast.year,
            time = MMWRweek2Date(year, mmwrWeek)) %>%
-    rename(`amblyomma_americanum` = density)
+    rename(ambloyomma_americanum = density)
   
   return(ens)
   
